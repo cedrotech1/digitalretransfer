45baaba307f553c103a22b2b8301ad9ae05d7dbb
cedrotech1 <cedrotech1@gmail.com>
2025-04-03 16:12:51 +0200

    commit1

--- src/pages/healthcenter.jsx
+++ src/pages/healthcenter.jsx
@@ -374,8 +374,8 @@ export default function HealthCenterManagement() {
                       {getSectorName(center.sectorId)}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {center.head.firstname}
-                      {center.head.lastname}
+                      {center?.head.firstname}
+                      {center?.head.lastname}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm font-medium space-x-2">
                       <button
